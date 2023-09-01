89777199e94864d1fc2731d73bfbbeb36f1807a8
Dylon-Chan <dylon_chan22@hotmail.com>
2023-09-02 02:38:15 +0800

    update env_var and staging TF deployment

--- environments/stage/stage.tf
+++ environments/stage/stage.tf
@@ -1,12 +1,13 @@
 resource "aws_security_group" "ecs_sg" {
   vpc_id = var.vpc_id
   name = var.ecs_sg_name
+  depends_on = [ aws_security_group.loadbalancer_sg ]
   
   ingress {
-    from_port = var.port
-    to_port = var.port
+    from_port = var.image_port
+    to_port = var.image_port
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.loadbalancer_sg.id]
   }
   
   egress {
@@ -31,11 +32,10 @@ resource "aws_ecs_task_definition" "task" {
   execution_role_arn = var.role_to_assume
   container_definitions = jsonencode([{
     name: "${var.ecs_name}-td",
-    image: "${var.image_name}",
+    image: "${var.image_name}:${var.image_tag}",
     portMappings: [
         {
-            containerPort: var.port,
-            hostPort: var.port,
+            containerPort: var.image_port,
             protocol: "tcp"
         }
     ],
@@ -48,12 +48,78 @@ resource "aws_ecs_service" "service" {
   cluster = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   launch_type = "FARGATE"
-  depends_on = [ aws_security_group.ecs_sg ]
+  depends_on = [ aws_security_group.ecs_sg, aws_lb_listener.front_end ]
   desired_count = 1
   
   network_configuration {
     subnets = var.subnets
     security_groups = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.load_balancer_tg.arn
+    container_name   = "${var.ecs_name}-td"
+    container_port   = var.image_port
+  }
+}
+
+# Below for Application Load Balancer
+
+resource "aws_security_group" "loadbalancer_sg" {
+  vpc_id = var.vpc_id
+  name = var.lb_sg_name
+  
+  ingress {
+    from_port = var.alb_port
+    to_port = var.alb_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_lb_target_group" "load_balancer_tg" {
+  name     = "group2-lb-tg-${var.env}"
+  port     = var.image_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 30
+    timeout = 3
+    healthy_threshold   = "3"
+    unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_lb" "loadbalancer" {
+  name               = "group2-loadbalancer-${var.env}"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.loadbalancer_sg.id]
+  subnets            = var.subnets
+
+  # enable_deletion_protection = true
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.loadbalancer.id
+  port              = var.alb_port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.load_balancer_tg.id
   }
 }
--- environments/stage/variables.tf
+++ environments/stage/variables.tf
@@ -16,10 +16,16 @@ variable "ecs_sg_name" {
   default = "group2-ecs-sg-stage"
 }
 
-variable "port" {
+variable "image_port" {
     description = "Port"
     type = number
-    default = 3030
+    default = 3000
+}
+
+variable "alb_port" {
+    description = "ALB Port"
+    type = number
+    default = 8000
 }
 
 variable "vpc_id" {
@@ -35,6 +41,14 @@ variable "subnets" {
 variable "image_name" {
   description = "ECR Repository Name"
   type = string
+  // image_name to be input at runtime
+  // default = "255945442255.dkr.ecr.ap-southeast-1.amazonaws.com/group2-chat-app" #for test locally
+}
+
+variable "image_tag" {
+  description = "Image Tag"
+  type = string
+  default = "latest"
 }
 
 variable "ecs_name" {
@@ -53,4 +67,10 @@ variable "memory" {
   description = "Memory"
   type = number
   default = 1024
+}
+
+variable "lb_sg_name" {
+  description = "ECS Security Group Name"
+  type = string
+  default = "group2-chat-app-lb-sg-stage"
 }
