e6b81ac3c386a846aadaebd95ac90e252011ee20
Dylon-Chan <dylon_chan22@hotmail.com>
2023-09-04 23:55:38 +0800

    update TF to force new deployment for ECS

--- environments/dev/dev.tf
+++ environments/dev/dev.tf
@@ -53,8 +53,9 @@ resource "aws_ecs_service" "service" {
   cluster = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   launch_type = "FARGATE"
-  depends_on = [ aws_security_group.ecs_sg ]
+  depends_on = [ aws_security_group.ecs_sg, aws_ecs_task_definition.task ]
   desired_count = 1
+  force_new_deployment = true
   
   network_configuration {
     subnets = var.subnets
--- environments/stage/stage.tf
+++ environments/stage/stage.tf
@@ -53,8 +53,9 @@ resource "aws_ecs_service" "service" {
   cluster = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   launch_type = "FARGATE"
-  depends_on = [ aws_security_group.ecs_sg, aws_lb_listener.front_end ]
+  depends_on = [ aws_security_group.ecs_sg, aws_ecs_task_definition.task, aws_lb_listener.front_end ]
   desired_count = 2
+  force_new_deployment = true
   
   network_configuration {
     subnets = var.subnets
